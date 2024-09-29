16964882835c33ef05b1a0fbc61b8728aff4d914
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-29 15:56:14 +0800

    style

---
+++ Sources/Rohan/equation/MathStyle.swift
@@ -0,0 +1,22 @@
+// Copyright 2024 Lie Yan
+
+///
+///
+/**
+ An extrinsic property of a math sub-formula.
+
+ # Note
+ The relation between math style and font size is as follows:
+
+ | Display Style   | Font Size          |
+ |-----------------|--------------------|
+ | Display, Text   | text size          |
+ | Script          | script size        |
+ | ScriptScript    | scriptscript size  |
+ */
+public enum MathStyle: Equatable, Hashable {
+    case Display
+    case Text
+    case Script
+    case ScriptScript
+}
---
+++ Sources/Rohan/equation/MathStyleUtils.swift
@@ -0,0 +1,62 @@
+// Copyright 2024 Lie Yan
+
+public enum MathStyleUtils {
+    // MARK: - Scripts
+
+    /**
+     Returns the script style of the given style.
+
+     # Functionality
+     Consider a sub-formula consisting of a nucleus and a superscript and/or a
+     subscript. Given the style of the sub-formula, returns the style of the
+     superscript and/or subscript.
+     */
+    public static func scriptStyle(of style: MathStyle) -> MathStyle {
+        switch style {
+        case .Display,
+             .Text:
+            return .Script
+        case .Script:
+            return .ScriptScript
+        case .ScriptScript:
+            return .ScriptScript
+        }
+    }
+
+    public static func superscriptCramped(of cramped: Bool) -> Bool {
+        cramped
+    }
+
+    public static func subscriptCramped(of cramped: Bool) -> Bool {
+        true
+    }
+
+    // MARK: - Fraction
+
+    /**
+     Returns the numerator/denominator style of the given style.
+
+     # Functionality
+     Consider a fraction consisting of a numerator and a denominator.
+     Given the style of the fraction, returns the style of the numerator/denominator.
+     */
+    public static func fractionStyle(of style: MathStyle) -> MathStyle {
+        switch style {
+        case .Display:
+            return .Text
+        case .Text:
+            return .Script
+        case .Script,
+             .ScriptScript:
+            return .ScriptScript
+        }
+    }
+
+    public static func numeratorCramped(of cramped: Bool) -> Bool {
+        cramped
+    }
+
+    public static func denominatorCramped(of cramped: Bool) -> Bool {
+        true
+    }
+}
