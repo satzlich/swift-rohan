b07c7d46529eafc30347189c3c9f171f0fe02140
Lie Yan <robyn.lie.yan@outlook.com>
2024-09-30 18:39:09 +0800

    pol

--- Sources/Rohan/Selection.swift
+++
@@ -1,5 +0,0 @@
-// Copyright 2024 Lie Yan
-
-public struct Selection {
-    
-}
--- Sources/Rohan/math/MathStyleUtils.swift
+++ Sources/Rohan/math/MathStyleUtils.swift
@@ -1,10 +1,21 @@
 // Copyright 2024 Lie Yan
 
 /**
- By default, a non-root formula adopts the same style as its parent.
- All exceptions are defined in the utility functions provided here.
+ Utility functions for math style
 
- > Cramped or uncramped: A non-root formula inherits the cramped-ness of its parent,
+ # Full Math Style
+
+ A formula's style is paired with a boolean state, indicating whether it is **cramped**
+ or **uncramped**. To fully describe the style of a formula, we use a value of type
+ `(MathStyle, Bool)`. By default, a non-root formula inherits this value from its parent.
+
+ Exceptions related to the `MathStyle` component are defined by the utility functions
+ provided here, while exceptions for the `Bool` (cramped or uncramped) component are
+ outlined below.
+
+ > To be or not to be (cramped)?:
+ >
+ > A non-root formula inherits its cramped or uncramped state from its parent,
  > except when it occupies one of the following positions:
  > - as a subscript,
  > - the denominator of a fraction,
@@ -12,7 +23,7 @@
  > - the nucleus of `\overline`, or
  > - the radicand of `\sqrt`.
  >
- > In these cases, it always switches to a cramped style.
+ > In these cases, it always switches to a **cramped** style.
 
  */
 public enum MathStyleUtils {
