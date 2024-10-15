a98d260cccfb7f17f92f3131c62449dc05c6bea2
Lie Yan <robyn.lie.yan@outlook.com>
2024-10-15 18:17:49 +0800

    cleanse

--- Tests/RohanTests/EndiannessTests.swift
+++ Tests/RohanTests/EndiannessTests.swift
@@ -6,15 +6,14 @@ import XCTest
 
 final class EndiannessTests: XCTestCase {
     func testUnicodeScalar() {
-        let character: Character = "😀"
+        let unicodeScalar: UnicodeScalar = "😀"
 
-        let unichars = character.utf16.map { $0 }
+        let unichars = unicodeScalar.utf16.map { $0 }
 
         XCTAssertEqual(unichars[0], 55357)
         XCTAssertEqual(unichars[1], 56832)
 
         let combinedValue = UTF16.combineSurrogates(unichars[0], unichars[1])
-
-        XCTAssertEqual(Character(UnicodeScalar(combinedValue)!), character)
+        XCTAssertEqual(UnicodeScalar(combinedValue)!, unicodeScalar)
     }
 }
