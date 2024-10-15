59031646222aa45ee1c4de4f8446b59694a0c99f
Lie Yan <robyn.lie.yan@outlook.com>
2024-10-15 16:04:54 +0800

    doc

--- Sources/Rohan/data-types/PropertyValue.swift
+++ Sources/Rohan/data-types/PropertyValue.swift
@@ -3,15 +3,21 @@
 import Foundation
 
 enum PropertyValue: Equatable, Hashable {
+    // basic types
+
     case bool(Bool)
     case int(Int)
     case float(Double)
     case string(String)
 
+    // font properties
+
     case fontSize(FontSize)
     case fontStyle(FontStyle)
     case fontWeight(FontWeight)
     case fontStretch(FontStretch)
+
+    // math properties
 
     case mathStyle(MathStyle)
     case mathVariant(MathVariant)
