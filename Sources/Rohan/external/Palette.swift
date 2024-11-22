70047fc2ad0704a3a8d52bc8011bac2c7cc78ba4
Lie Yan <robyn.lie.yan@outlook.com>
2024-11-22 21:52:43 +0800

    color

--- Sources/Rohan/external/Palette.swift
+++ Sources/Rohan/external/Palette.swift
@@ -10,14 +10,14 @@ enum Palette {
         let blue: Double
         let alpha: Double
 
-        init(red: Double, green: Double, blue: Double, alpha: Double) {
+        public init(red: Double, green: Double, blue: Double, alpha: Double) {
             self.red = red.clamped(0, 1)
             self.green = green.clamped(0, 1)
             self.blue = blue.clamped(0, 1)
             self.alpha = alpha.clamped(0, 1)
         }
 
-        var cgColor: CGColor {
+        public var cgColor: CGColor {
             CGColor(
                 red: red,
                 green: green,
