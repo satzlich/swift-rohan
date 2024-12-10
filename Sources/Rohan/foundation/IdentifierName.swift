cacee70379df488661476ae3544ff479f763d0ea
Lie Yan <robyn.lie.yan@outlook.com>
2024-12-10 11:50:23 +0800

    rename

--- Sources/Rohan/foundation/IdentifierName.swift
+++ Sources/Rohan/foundation/IdentifierName.swift
@@ -6,14 +6,14 @@ struct IdentifierName: Equatable, Hashable, Codable {
     let name: String
 
     init?(_ name: String) {
-        guard IdentifierName.validateIdentifierName(name) else {
+        guard IdentifierName.validateName(name) else {
             return nil
         }
 
         self.name = name
     }
 
-    static func validateIdentifierName(_ text: String) -> Bool {
-        try! #/[a-zA-Z_][a-zA-Z0-9_]*/#.wholeMatch(in: text) != nil
+    static func validateName(_ string: String) -> Bool {
+        try! #/[a-zA-Z_][a-zA-Z0-9_]*/#.wholeMatch(in: string) != nil
     }
 }
