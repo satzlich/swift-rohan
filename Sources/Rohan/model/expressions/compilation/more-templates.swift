8d7911c58aec8a47069d873bd484ab0668cfb2ab
Lie Yan <robyn.lie.yan@outlook.com>
2024-12-12 19:32:32 +0800

    add validator

--- Sources/Rohan/model/expressions/compilation/more-templates.swift
+++ Sources/Rohan/model/expressions/compilation/more-templates.swift
@@ -34,6 +34,51 @@ struct NamelessTemplate {
         // contains no apply
         // variables are nameless
         // variable indices are in range
-        true
+        let context = Context(parameterCount: parameterCount)
+        Validator().visitContent(body, context)
+        return context.okay
+    }
+
+    final class Context {
+        var applyCount = 0 // named and nameless
+        var namedVariableCount = 0
+        var namelessVariable_OutOfRange_Count = 0
+        let parameterCount: Int
+
+        var okay: Bool {
+            applyCount == 0 &&
+                namedVariableCount == 0 &&
+                namelessVariable_OutOfRange_Count == 0
+        }
+
+        init(parameterCount: Int) {
+            self.parameterCount = parameterCount
+        }
+    }
+
+    final class Validator: ExpressionVisitor<Context> {
+        override func visitApply(_ apply: Apply, _ context: Context) {
+            context.applyCount += 1
+            super.visitApply(apply, context)
+        }
+
+        override func visitNamelessApply(
+            _ namelessApply: NamelessApply, _ context: Context
+        ) {
+            context.applyCount += 1
+            super.visitNamelessApply(namelessApply, context)
+        }
+
+        override func visitVariable(_ variable: Variable, _ context: Context) {
+            context.namedVariableCount += 1
+            super.visitVariable(variable, context)
+        }
+
+        override func visitNamelessVariable(_ namelessVariable: NamelessVariable, _ context: Context) {
+            if namelessVariable.index >= context.parameterCount {
+                context.namelessVariable_OutOfRange_Count += 1
+            }
+            super.visitNamelessVariable(namelessVariable, context)
+        }
     }
 }
