228f1571dcff417e334e5dc8942e2887779e4fe3
Lie Yan <robyn.lie.yan@outlook.com>
2024-12-14 00:05:13 +0800

    rename

--- Sources/Rohan/model/expressions/AnnotatedTemplate.swift
+++ Sources/Rohan/model/expressions/AnnotatedTemplate.swift
@@ -1,7 +1,6 @@
 // Copyright 2024 Lie Yan
 
 import Collections
-import Foundation
 
 struct AnnotatedTemplate<A> {
     let canonical: Template
@@ -23,22 +22,3 @@ typealias TemplateUses = Set<TemplateName>
  variable name -> variable use paths
  */
 typealias VariableUses = OrderedDictionary<Identifier, OrderedSet<TreePath>>
-
-struct NamelessTemplate {
-    let parameterCount: Int
-    let body: Content
-
-    /**
-
-     ## Conditions to check
-     * contains no apply, named or nameless;
-     * variables are nameless;
-     * variable indices are in range
-
-     */
-    public static func validateBody(_ body: Content,
-                                    _ parameterCount: Int) -> Bool
-    {
-        false
-    }
-}
