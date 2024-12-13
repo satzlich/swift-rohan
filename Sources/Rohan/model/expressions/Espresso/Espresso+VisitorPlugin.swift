1f3913c7527d68ac4dc2fdd419d08a8629c9b935
Lie Yan <robyn.lie.yan@outlook.com>
2024-12-13 19:19:07 +0800

    simplify PluginVisitor

--- Sources/Rohan/model/expressions/Espresso/Espresso+VisitorPlugin.swift
+++ Sources/Rohan/model/expressions/Espresso/Espresso+VisitorPlugin.swift
@@ -8,35 +8,9 @@ extension Espresso {
         typealias Context = Void
 
         mutating func visitExpression(_ expression: Expression, _ context: Context)
-        mutating func visitApply(_ apply: Apply, _ context: Context)
-        mutating func visitVariable(_ variable: Variable, _ context: Context)
-        mutating func visitNamelessApply(_ namelessApply: NamelessApply, _ context: Context)
-        mutating func visitNamelessVariable(_ namelessVariable: NamelessVariable, _ context: Context)
-        mutating func visitText(_ text: Text, _ context: Context)
-        mutating func visitContent(_ content: Content, _ context: Context)
-        mutating func visitEmphasis(_ emphasis: Emphasis, _ context: Context)
-        mutating func visitHeading(_ heading: Heading, _ context: Context)
-        mutating func visitParagraph(_ paragraph: Paragraph, _ context: Context)
-        mutating func visitEquation(_ equation: Equation, _ context: Context)
-        mutating func visitFraction(_ fraction: Fraction, _ context: Context)
-        mutating func visitMatrix(_ matrix: Matrix, _ context: Context)
-        mutating func visitScripts(_ scripts: Scripts, _ context: Context)
     }
 }
 
 extension Espresso.VisitorPlugin {
     mutating func visitExpression(_ expression: Expression, _ context: Context) { }
-    mutating func visitApply(_ apply: Apply, _ context: Context) { }
-    mutating func visitVariable(_ variable: Variable, _ context: Context) { }
-    mutating func visitNamelessApply(_ namelessApply: NamelessApply, _ context: Context) { }
-    mutating func visitNamelessVariable(_ namelessVariable: NamelessVariable, _ context: Context) { }
-    mutating func visitText(_ text: Text, _ context: Context) { }
-    mutating func visitContent(_ content: Content, _ context: Context) { }
-    mutating func visitEmphasis(_ emphasis: Emphasis, _ context: Context) { }
-    mutating func visitHeading(_ heading: Heading, _ context: Context) { }
-    mutating func visitParagraph(_ paragraph: Paragraph, _ context: Context) { }
-    mutating func visitEquation(_ equation: Equation, _ context: Context) { }
-    mutating func visitFraction(_ fraction: Fraction, _ context: Context) { }
-    mutating func visitMatrix(_ matrix: Matrix, _ context: Context) { }
-    mutating func visitScripts(_ scripts: Scripts, _ context: Context) { }
 }
