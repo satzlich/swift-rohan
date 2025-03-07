de866c30021d10f8e3587d805765740f1655bd22
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-07 15:59:46 +0800

    pol

--- Sources/Rohan/TemplateSystem/Expressions/RohanIndex.swift
+++ Sources/Rohan/TemplateSystem/Expressions/RohanIndex.swift
@@ -50,68 +50,66 @@ public enum RohanIndex: Equatable, Hashable, CustomStringConvertible {
     case let .argumentIndex(index): return "\(index)⇒"
     }
   }
+}
 
-  public enum MathIndex: Int, Comparable, CustomStringConvertible {
-    case nucleus = 0
-    // scripts
-    case subScript = 1
-    case superScript = 2
-    // fraction
-    case numerator = 3
-    case denominator = 4
-    // radical
-    case index = 5
-    case radicand = 6
+public enum MathIndex: Int, Comparable, CustomStringConvertible {
+  case nucleus = 0
+  // scripts
+  case subScript = 1
+  case superScript = 2
+  // fraction
+  case numerator = 3
+  case denominator = 4
+  // radical
+  case index = 5
+  case radicand = 6
 
-    public static func < (lhs: MathIndex, rhs: MathIndex) -> Bool {
-      lhs.rawValue < rhs.rawValue
-    }
-
-    public var description: String {
-      switch self {
-      case .nucleus: return "nucleus"
-      case .subScript: return "subscript"
-      case .superScript: return "superscript"
-      case .numerator: return "numerator"
-      case .denominator: return "denominator"
-      case .index: return "index"
-      case .radicand: return "radicand"
-      }
-    }
+  public static func < (lhs: MathIndex, rhs: MathIndex) -> Bool {
+    lhs.rawValue < rhs.rawValue
   }
 
-  public struct GridIndex: Hashable, Comparable, CustomStringConvertible {
-    public let row: Int
-    public let column: Int
-
-    internal init(_ row: Int, _ column: Int) {
-      precondition(GridIndex.validate(row: row))
-      precondition(GridIndex.validate(column: column))
-      self.row = row
-      self.column = column
-    }
-
-    public var description: String {
-      "(\(row), \(column))"
-    }
-
-    public static func < (lhs: GridIndex, rhs: GridIndex) -> Bool {
-      (lhs.row, lhs.column) < (rhs.row, rhs.column)
-    }
-
-    /*
-     We follow the practice of Microsoft Word.
-     Row count must be between 1 and 32767. Column count must be between 1 and 63.
-     */
-
-    internal static func validate(row: Int) -> Bool {
-      0..<32767 ~= row
-    }
-
-    internal static func validate(column: Int) -> Bool {
-      0..<63 ~= column
+  public var description: String {
+    switch self {
+    case .nucleus: return "nucleus"
+    case .subScript: return "subscript"
+    case .superScript: return "superscript"
+    case .numerator: return "numerator"
+    case .denominator: return "denominator"
+    case .index: return "index"
+    case .radicand: return "radicand"
     }
   }
 }
 
-public typealias MathIndex = RohanIndex.MathIndex
+public struct GridIndex: Hashable, Comparable, CustomStringConvertible {
+  public let row: Int
+  public let column: Int
+
+  internal init(_ row: Int, _ column: Int) {
+    precondition(GridIndex.validate(row: row))
+    precondition(GridIndex.validate(column: column))
+    self.row = row
+    self.column = column
+  }
+
+  public var description: String {
+    "(\(row), \(column))"
+  }
+
+  public static func < (lhs: GridIndex, rhs: GridIndex) -> Bool {
+    (lhs.row, lhs.column) < (rhs.row, rhs.column)
+  }
+
+  /*
+   We follow the practice of Microsoft Word.
+   Row count must be between 1 and 32767. Column count must be between 1 and 63.
+   */
+
+  internal static func validate(row: Int) -> Bool {
+    0..<32767 ~= row
+  }
+
+  internal static func validate(column: Int) -> Bool {
+    0..<63 ~= column
+  }
+}
