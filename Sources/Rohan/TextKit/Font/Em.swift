5ffa754d0d0d7476731cf28e6d1e1b6700d26e32
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-11 18:22:57 +0800

    cl

---
+++ Sources/Rohan/TextKit/Font/Em.swift
@@ -0,0 +1,21 @@
+// Copyright 2024-2025 Lie Yan
+
+import Foundation
+
+struct Em: Equatable, Hashable, Sendable {
+  let floatValue: Double
+
+  init(_ floatValue: Double) {
+    precondition(floatValue.isFinite)
+    self.floatValue = floatValue
+  }
+
+  static var zero: Em { Em(0.0) }
+
+  // spacing
+  static var thin: Em { Em(1.0 / 6.0) }
+  static var medium: Em { Em(2.0 / 9.0) }
+  static var thick: Em { Em(5.0 / 18.0) }
+  static var quad: Em { Em(1.0) }
+  static var wide: Em { Em(2.0) }
+}
