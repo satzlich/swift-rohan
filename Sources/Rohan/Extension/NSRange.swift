634a3f621c47317d046f509d47502de539b1a7df
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-13 10:24:50 +0800

    mv

---
+++ Sources/Rohan/Extension/NSRange.swift
@@ -0,0 +1,26 @@
+// Copyright 2024-2025 Lie Yan
+
+import AppKit
+
+extension NSRange {
+  /**
+   Clamp the range to another range.
+
+   - Returns: The clamped range if both ranges are valid. Otherwise, return a value
+      whose `location` equals `NSNotFound`.
+   */
+  func clamped(to range: NSRange) -> NSRange {
+    // if one of the ranges is invalid, return an invalid range
+    if self.location == NSNotFound || range.location == NSNotFound {
+      return .notFound
+    }
+    // otherwise, do the clamping
+    let lowerBound = range.lowerBound
+    let upperBound = range.upperBound
+    let location = self.location.clamped(lowerBound, upperBound)
+    let end = (self.location + self.length).clamped(lowerBound, upperBound)
+    return NSRange(location: location, length: end - location)
+  }
+
+  static var notFound: NSRange { NSRange(location: NSNotFound, length: 0) }
+}
