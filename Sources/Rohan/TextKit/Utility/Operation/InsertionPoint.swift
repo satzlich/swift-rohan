5d0a79209978d95dbee8a7a752ec9896b98b099d
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-16 18:07:08 +0800

    insertion point

--- Sources/Rohan/TextKit/Utility/Operation/InsertionPoint.swift
+++ Sources/Rohan/TextKit/Utility/Operation/InsertionPoint.swift
@@ -2,9 +2,7 @@
 
 import Foundation
 
-/**
- Return value of insert operations on document, representing the new insertion point.
- */
+/** Insertion point resulted from insert operation. */
 struct InsertionPoint {
   /** the new insertion point */
   let insertionPoint: TextLocation
@@ -17,5 +15,21 @@ struct InsertionPoint {
   init(_ insertionPoint: TextLocation, isSame: Bool) {
     self.insertionPoint = insertionPoint
     self.isSame = isSame
+  }
+
+  /**
+   Combine two successive insertion points into one.
+
+   - Invariant: Denote combine by ⊕. Given a sequence of insertion points
+      `p_1, p_2, ..., p_n`, then `p_1 ⊕ p_2 ⊕ ... ⊕ p_n` satisfies the following:
+      a) property `insertionPoint` equals `p_n.insertionPoint`;
+      b) property `isSame` equals `p_1.isSame ∧ p_2.isSame ∧ ... ∧ p_n.isSame`.
+   */
+  func combine(with next: InsertionPoint) -> InsertionPoint {
+    if next.isSame {
+      assert(insertionPoint == next.insertionPoint)
+      return self
+    }
+    return next
   }
 }
