41543d2963f2f4f36e86aa25157e38959b7680ea
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-16 18:10:28 +0800

    doc

--- Sources/Rohan/TextKit/NodeUtils/NodeUtils+InsertNodes.swift
+++ Sources/Rohan/TextKit/NodeUtils/NodeUtils+InsertNodes.swift
@@ -1,6 +1,10 @@
 // Copyright 2024-2025 Lie Yan
 
 extension NodeUtils {
+  /**
+   Insert nodes at the specified location in the tree.
+   - Returns: The new insertion point if the insertion is successful; otherwise, nil.
+   */
   static func insertNodes(
     _ nodes: [Node], at location: TextLocation, _ tree: RootNode
   ) -> InsertionPoint? {
