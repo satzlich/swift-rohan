5fb81a2b688cf0061e6b1bbbfb8625dbe6887327
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-25 12:29:53 +0800

    add undo

---
+++ Sources/Rohan/TextView/TextView+Undo.swift
@@ -0,0 +1,14 @@
+// Copyright 2024-2025 Lie Yan
+
+import AppKit
+import Foundation
+
+extension TextView {
+  @objc public func redo(_ sender: Any?) {
+    print("redo")
+  }
+
+  @objc public func undo(_ sender: Any?) {
+    print("undo")
+  }
+}
