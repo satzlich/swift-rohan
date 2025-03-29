9123df415a790e42164db9225b6798a93bcf6adc
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-29 11:06:02 +0800

    mv

---
+++ Sources/Rohan/TextView/TextView+Replace.swift
@@ -0,0 +1,13 @@
+// Copyright 2024-2025 Lie Yan
+
+import Foundation
+
+extension TextView {
+  func replaceContents(in range: RhTextRange, with nodes: [Node]?) {
+
+  }
+
+  func replaceCharacters(in range: RhTextRange, with string: String) {
+
+  }
+}
