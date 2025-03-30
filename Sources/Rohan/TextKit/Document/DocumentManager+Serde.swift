fd6dde0e7c1823fa92a73b763606be07cf55b061
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-30 13:49:05 +0800

    mv

--- Sources/Rohan/TextKit/Document/DocumentManager+Serde.swift
+++ Sources/Rohan/TextKit/Document/DocumentManager+Serde.swift
@@ -18,7 +18,7 @@ extension DocumentManager {
   }
 
   /// Serialize the contents in the given range to a lossy string.
-  /// -Returns: The lossy string, or nil if the range is invalid.
+  /// - Returns: The lossy string, or nil if the range is invalid.
   func stringify(for range: RhTextRange) -> BigString? {
     // obtain nodes in the range
     guard let nodes = mapContents(in: range, { $0 }) else { return nil }
