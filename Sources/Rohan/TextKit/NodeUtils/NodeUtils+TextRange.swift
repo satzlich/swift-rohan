d26698430bdf03fc2c8c879e60df630adf38994d
Lie Yan <robyn.lie.yan@outlook.com>
2025-03-30 22:10:20 +0800

    mv

--- Sources/Rohan/TextKit/NodeUtils/NodeUtils+TextRange.swift
+++ Sources/Rohan/TextKit/NodeUtils/NodeUtils+TextRange.swift
@@ -172,49 +172,4 @@ extension NodeUtils {
       return validateOffset(range.endLocation.offset, trace.last!.node)
     }
   }
-
-  /// Returns true if the offset is valid for the node.
-  static func validateOffset(_ offset: Int, _ node: Node) -> Bool {
-    switch node {
-    case let textNode as TextNode:
-      return (0...textNode.length) ~= offset
-    case let elementNode as ElementNode:
-      return (0...elementNode.childCount) ~= offset
-    case let argumentNode as ArgumentNode:
-      return (0...argumentNode.childCount) ~= offset
-    default:
-      return false
-    }
-  }
-
-  /// Compute the visual delimiter range for a location in the tree.
-  static func visualDelimiterRange(
-    for location: TextLocation, _ tree: RootNode
-  ) -> RhTextRange? {
-    guard let trace = Trace.from(location, tree) else { return nil }
-
-    // find the last non-transparent node
-    let i = trace.lastIndex(where: { $0.node.isTransparent == false })
-    guard let i else { return nil }
-
-    // check if the node needs visual delimiter
-    let node = trace[i].node
-    guard NodePolicy.needsVisualDelimiter(node.type) else { return nil }
-
-    // take prefix
-    let prefix = trace[0..<i].map(\.index)
-
-    switch node {
-    case let element as ElementNode:
-      let end = element.childCount
-      guard end > 0 else { return nil }
-      return RhTextRange(TextLocation(prefix, 0), TextLocation(prefix, end))
-    case let argument as ArgumentNode:
-      let end = argument.childCount
-      guard end > 0 else { return nil }
-      return RhTextRange(TextLocation(prefix, 0), TextLocation(prefix, end))
-    default:
-      return nil
-    }
-  }
 }
