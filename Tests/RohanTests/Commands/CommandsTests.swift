311dfd559764d21f3f46916dfbdf4c41fbe92cdf
Lie Yan <robyn.lie.yan@outlook.com>
2025-04-11 10:11:46 +0800

    test

---
+++ Tests/RohanTests/Commands/CommandsTests.swift
@@ -0,0 +1,14 @@
+// Copyright 2024-2025 Lie Yan
+
+import Foundation
+import Testing
+
+@testable import Rohan
+
+struct CommandsTests {
+
+  @Test
+  static func testDefaultCommands() {
+    #expect(DefaultCommands.allCases.count == 213)
+  }
+}
