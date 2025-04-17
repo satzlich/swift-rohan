4625bca6a8d9947ee625a34f06b0799c61626ad6
Lie Yan <robyn.lie.yan@outlook.com>
2025-04-17 22:00:51 +0800

    mod

--- Sources/Rohan/TextView/CompositorWindow/CompositorStyle.swift
+++ Sources/Rohan/TextView/CompositorWindow/CompositorStyle.swift
@@ -10,30 +10,22 @@ enum CompositorStyle {
   static let iconSize: CGFloat = 18
   static let font = NSFont.monospacedSystemFont(ofSize: fontSize, weight: .regular)
 
+  // MARK: - Attributes
+
   static let baseAttrs: [NSAttributedString.Key: Any] = [.font: font]
+
   static let emphAttrs: [NSAttributedString.Key: Any] =
     [.font: NSFont.monospacedSystemFont(ofSize: fontSize, weight: .bold)]
 
-  static func previewAttrs(mathMode: Bool = false) -> [NSAttributedString.Key: Any] {
+  static func previewAttrs(mathMode: Bool) -> [NSAttributedString.Key: Any] {
     mathMode ? mathPreviewAttrs : previewAttrs
   }
+
   private static let mathPreviewAttrs: [NSAttributedString.Key: Any] =
     [.font: mathPreviewFont(iconSize)]
+
   private static let previewAttrs: [NSAttributedString.Key: Any] =
     [.font: NSFont.systemFont(ofSize: iconSize)]
-
-  static let leadingPadding: CGFloat = 6
-  static let trailingPadding: CGFloat = 14
-  /// content inset for scroll view
-  static let contentInset: CGFloat = 6
-  /// padding for icon size difference
-  static let iconDiff: CGFloat = 1.5
-  /// spacing between text and icon
-  static let iconTextSpacing: CGFloat = 7
-  private static let unknownError: CGFloat = 2
-
-  static let textFieldXOffset: CGFloat =
-    contentInset + leadingPadding + iconSize + iconDiff + iconTextSpacing + unknownError
 
   private static func mathPreviewFont(_ fontSize: CGFloat) -> NSFont {
     if let font = NSFont(name: "Latin Modern Math", size: fontSize) {
@@ -44,4 +36,22 @@ enum CompositorStyle {
         ?? NSFont.systemFont(ofSize: fontSize)
     }
   }
+
+  // MARK: - Metric
+
+  static let leadingPadding: CGFloat = 6
+  static let trailingPadding: CGFloat = 14
+
+  /// content inset for scroll view
+  static let contentInset: CGFloat = 6
+
+  /// padding for icon size difference
+  static let iconDiff: CGFloat = 1.5
+
+  /// spacing between text and icon
+  static let iconTextSpacing: CGFloat = 7
+  private static let unknownError: CGFloat = 2
+
+  static let textFieldXOffset: CGFloat =
+    contentInset + leadingPadding + iconSize + iconDiff + iconTextSpacing + unknownError
 }
