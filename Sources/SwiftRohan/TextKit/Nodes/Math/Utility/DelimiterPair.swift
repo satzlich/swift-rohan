858c835cd0301ee9cc4a3fc3cd7eba2bab4617d5
Lie Yan <robyn.lie.yan@outlook.com>
2025-04-26 12:31:58 +0800

    add delimiter pair

---
+++ Sources/SwiftRohan/TextKit/Nodes/Math/Utility/DelimiterPair.swift
@@ -0,0 +1,70 @@
+// Copyright 2024-2025 Lie Yan
+
+import Foundation
+import UnicodeMathClass
+
+struct Delimiter {
+  let value: Optional<Character>
+
+  init() { value = nil }
+
+  init?(_ char: Character) {
+    guard Delimiter.validate(char) else { return nil }
+    self.value = char
+  }
+
+  /// Returns the matching delimiter for the current one.
+  func matchingDelimiter() -> Delimiter? {
+    switch value {
+    case .none: return nil
+    case .some("["): return Delimiter("]")
+    case .some("]"): return Delimiter("[")
+    case .some("{"): return Delimiter("}")
+    case .some("}"): return Delimiter("{")
+    case .some(let char):
+      let unicodeScalar = char.unicodeScalars.first!
+      switch unicodeScalar.mathClass {
+      case .Opening:
+        let value = unicodeScalar.value + 1
+        return Delimiter(Character(UnicodeScalar(value)!))
+      case .Closing:
+        let value = unicodeScalar.value - 1
+        return Delimiter(Character(UnicodeScalar(value)!))
+      default:
+        return self
+      }
+    }
+  }
+
+  static func validate(_ character: Character) -> Bool {
+    let clazz = character.unicodeScalars.first!.mathClass
+    return [.Opening, .Closing, .Fence].contains(clazz)
+  }
+}
+
+/// A pair of delimiters (one closing, one opening) used for matrices, vectors
+/// and cases.
+struct DelimiterPair {
+  let open: Delimiter
+  let close: Delimiter
+
+  init(_ open: Delimiter, _ close: Delimiter) {
+    self.open = open
+    self.close = close
+  }
+
+  init?(_ open: Character, _ close: Character) {
+    guard let open = Delimiter(open),
+      let close = Delimiter(close)
+    else { return nil }
+
+    self.open = open
+    self.close = close
+  }
+}
+
+extension DelimiterPair {
+  static let PAREN = DelimiterPair("(", ")")!
+  static let BRACE = DelimiterPair("{", "}")!
+  static let BRACKET = DelimiterPair("[", "]")!
+}
