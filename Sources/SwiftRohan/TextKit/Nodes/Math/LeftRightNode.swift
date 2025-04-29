68602018298eaea615900cab15e56c7a6e1e824f
Lie Yan <robyn.lie.yan@outlook.com>
2025-04-29 15:42:54 +0800

    fix picking

--- Sources/SwiftRohan/TextKit/Nodes/Math/LeftRightNode.swift
+++ Sources/SwiftRohan/TextKit/Nodes/Math/LeftRightNode.swift
@@ -125,11 +125,11 @@ final class LeftRightNode: MathNode {
   override func getMathIndex(interactingAt point: CGPoint) -> MathIndex? {
     guard let fragment = _leftRightFragment else { return nil }
 
-    let x1 = (fragment.minX + fragment.nucleus.minX) / 2
+    let x1 = (0 + fragment.nucleus.minX) / 2
     if point.x < x1 {
       return nil
     }
-    let x2 = (fragment.maxX + fragment.nucleus.maxX) / 2
+    let x2 = (fragment.width + fragment.nucleus.maxX) / 2
     if point.x > x2 {
       return nil
     }
