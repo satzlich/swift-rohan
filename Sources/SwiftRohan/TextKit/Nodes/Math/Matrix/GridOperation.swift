a3c3603945693a17e30376c6d309ea84c4e37a17
Lie Yan <robyn.lie.yan@outlook.com>
2025-05-11 13:23:58 +0800

    doc

--- Sources/SwiftRohan/TextKit/Nodes/Math/Matrix/GridOperation.swift
+++ Sources/SwiftRohan/TextKit/Nodes/Math/Matrix/GridOperation.swift
@@ -1,5 +1,6 @@
 // Copyright 2024-2025 Lie Yan
 
+/// Operations over Grid node.
 internal enum GridOperation {
   case insertRow(_ elements: Array<Array<Node>>, at: Int)
   case insertColumn(_ elements: Array<Array<Node>>, at: Int)
