67e53dc71419335effa43028261706c7e6aa7c39
Lie Yan <robyn.lie.yan@outlook.com>
2025-05-21 22:51:17 +0800

    add composite

--- Sources/SwiftRohan/Foundation/Math/MathUtils+Tables.swift
+++ Sources/SwiftRohan/Foundation/Math/MathUtils+Tables.swift
@@ -11,8 +11,9 @@ extension MathUtils {
     "\u{003A}": .Relation,  // COLON used as ratio mark. (Use \colon instead for punctuation.)
     "\u{2020}": .Normal,  // \dag (Use \dagger instead for binary operator.)
     "\u{2021}": .Normal,  // \ddag (User \ddagger instead for binary operator.)
-    "\u{2216}": .Binary,  // SET MINUS
     "\u{2026}": .Binary,  // \ldots
+    "\u{2216}": .Binary,  // SET MINUS
+    "\u{25EF}": .Binary,  // LARGE CIRCLE
   ]
 
   // substitution table for characters
