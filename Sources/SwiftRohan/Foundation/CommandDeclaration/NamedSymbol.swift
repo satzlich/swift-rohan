c1575f46de329ff9a1c1501454dc7ea1539ce1c8
Lie Yan <robyn.lie.yan@outlook.com>
2025-05-22 12:44:29 +0800

    mv

--- Sources/SwiftRohan/Foundation/CommandDeclaration/NamedSymbol.swift
+++ Sources/SwiftRohan/Foundation/CommandDeclaration/NamedSymbol.swift
@@ -442,6 +442,51 @@ private enum LaTeXCommands {
     // .init("Relbar", "="), // turned off
   ]
 
+  // total: 41 symbols
+  private static let greekLetters: Array<NamedSymbol> = [
+    .init("alpha", "\u{03B1}"),  // α
+    .init("beta", "\u{03B2}"),  // β
+    .init("gamma", "\u{03B3}"),  // γ
+    .init("delta", "\u{03B4}"),  // δ
+    .init("epsilon", "\u{03F5}"),  // ϵ
+    .init("varepsilon", "\u{03B5}"),  // ε
+    .init("zeta", "\u{03B6}"),  // ζ
+    .init("eta", "\u{03B7}"),  // η
+    .init("theta", "\u{03B8}"),  // θ
+    .init("vartheta", "\u{03D1}"),  // ϑ
+    .init("iota", "\u{03B9}"),  // ι
+    .init("kappa", "\u{03BA}"),  // κ
+    .init("lambda", "\u{03BB}"),  // λ
+    .init("mu", "\u{03BC}"),  // μ
+    .init("nu", "\u{03BD}"),  // ν
+    .init("xi", "\u{03BE}"),  // ξ
+    // omicron is not defined
+    .init("pi", "\u{03C0}"),  // π
+    .init("varpi", "\u{03D6}"),  // ϖ
+    .init("rho", "\u{03C1}"),  // ρ
+    .init("varrho", "\u{03F1}"),  // ϱ
+    .init("sigma", "\u{03C3}"),  // σ
+    .init("varsigma", "\u{03C2}"),  // ς
+    .init("tau", "\u{03C4}"),  // τ
+    .init("upsilon", "\u{03C5}"),  // υ
+    .init("phi", "\u{03D5}"),  // ϕ
+    .init("varphi", "\u{03C6}"),  // φ
+    .init("chi", "\u{03C7}"),  // χ
+    .init("psi", "\u{03C8}"),  // ψ
+    .init("omega", "\u{03C9}"),  // ω
+    .init("Gamma", "\u{0393}"),  // Γ
+    .init("Delta", "\u{0394}"),  // Δ
+    .init("Theta", "\u{0398}"),  // Θ
+    .init("Lambda", "\u{039B}"),  // Λ
+    .init("Xi", "\u{039E}"),  // Ξ
+    .init("Pi", "\u{03A0}"),  // Π
+    .init("Sigma", "\u{03A3}"),  // Σ
+    .init("Upsilon", "\u{03A5}"),  // Υ
+    .init("Phi", "\u{03A6}"),  // Φ
+    .init("Psi", "\u{03A8}"),  // Ψ
+    .init("Omega", "\u{03A9}"),  // Ω
+  ]
+
   private static let relation: Array<NamedSymbol> = [
     .init("ni", "\u{220B}"),  // ∋
     .init("owns", "\u{220B}"),  // ∋
@@ -478,50 +523,6 @@ private enum LaTeXCommands {
     .init("rmoustache", "\u{23B1}"),  // ⎱
     .init("lgroup", "\u{27EE}"),  // ⟮
     .init("rgroup", "\u{27EF}"),  // ⟯
-  ]
-
-  private static let greekLetters: Array<NamedSymbol> = [
-    .init("alpha", "\u{03B1}"),  // α
-    .init("beta", "\u{03B2}"),  // β
-    .init("gamma", "\u{03B3}"),  // γ
-    .init("delta", "\u{03B4}"),  // δ
-    .init("epsilon", "\u{03F5}"),  // ϵ
-    .init("zeta", "\u{03B6}"),  // ζ
-    .init("eta", "\u{03B7}"),  // η
-    .init("theta", "\u{03B8}"),  // θ
-    .init("iota", "\u{03B9}"),  // ι
-    .init("kappa", "\u{03BA}"),  // κ
-    .init("lambda", "\u{03BB}"),  // λ
-    .init("mu", "\u{03BC}"),  // μ
-    .init("nu", "\u{03BD}"),  // ν
-    .init("xi", "\u{03BE}"),  // ξ
-    .init("omicron", "\u{03BF}"),  // ο
-    .init("pi", "\u{03C0}"),  // π
-    .init("rho", "\u{03C1}"),  // ρ
-    .init("sigma", "\u{03C3}"),  // σ
-    .init("tau", "\u{03C4}"),  // τ
-    .init("upsilon", "\u{03C5}"),  // υ
-    .init("phi", "\u{03D5}"),  // ϕ
-    .init("chi", "\u{03C7}"),  // χ
-    .init("psi", "\u{03C8}"),  // ψ
-    .init("omega", "\u{03C9}"),  // ω
-    .init("varepsilon", "\u{03B5}"),  // ε
-    .init("vartheta", "\u{03D1}"),  // ϑ
-    .init("varpi", "\u{03D6}"),  // ϖ
-    .init("varrho", "\u{03F1}"),  // ϱ
-    .init("varsigma", "\u{03C2}"),  // ς
-    .init("varphi", "\u{03C6}"),  // φ
-    .init("Gamma", "\u{0393}"),  // Γ
-    .init("Delta", "\u{0394}"),  // Δ
-    .init("Theta", "\u{0398}"),  // Θ
-    .init("Lambda", "\u{039B}"),  // Λ
-    .init("Xi", "\u{039E}"),  // Ξ
-    .init("Pi", "\u{03A0}"),  // Π
-    .init("Sigma", "\u{03A3}"),  // Σ
-    .init("Upsilon", "\u{03A5}"),  // Υ
-    .init("Phi", "\u{03A6}"),  // Φ
-    .init("Psi", "\u{03A8}"),  // Ψ
-    .init("Omega", "\u{03A9}"),  // Ω
   ]
 
   private static let other: Array<NamedSymbol> = [
