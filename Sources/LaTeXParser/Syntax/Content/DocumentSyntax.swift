568293d71dbc5035c7cc9f5cf6bda9c42072cc4a
Lie Yan <robyn.lie.yan@outlook.com>
2025-05-30 16:58:00 +0800

    export

--- Sources/LaTeXParser/Syntax/Content/DocumentSyntax.swift
+++ Sources/LaTeXParser/Syntax/Content/DocumentSyntax.swift
@@ -6,10 +6,29 @@ public struct DocumentSyntax: SyntaxProtocol {
   public init(_ stream: StreamSyntax) {
     self.stream = stream
   }
+
+  public var preamble = TextSyntax(
+    #"""
+    \documentclass[10pt]{article}
+    \usepackage[usenames]{color}
+    \usepackage{amssymb}
+    \usepackage{amsmath}
+    \usepackage[utf8]{inputenc} 
+    \usepackage{unicode-math}
+
+    \begin{document}
+
+    """#, mode: .rawMode)!
+
+  public var postamble = TextSyntax(
+    #"""
+
+    \end{document}
+    """#, mode: .rawMode)!
 }
 
 extension DocumentSyntax {
   public func deparse() -> Array<any TokenProtocol> {
-    stream.deparse()
+    [preamble] + stream.deparse() + [postamble]
   }
 }
