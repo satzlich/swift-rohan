8bceaec42799dfae740fb5130cb3f80213a6520c
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-03 17:57:50 +0800

    pol

--- Tests/RohanTests/TextKit/Nodes/CriticalCare/PerformLayout/MathNodeLayoutTests.swift
+++ Tests/RohanTests/TextKit/Nodes/CriticalCare/PerformLayout/MathNodeLayoutTests.swift
@@ -7,5 +7,16 @@ import Testing
 @testable import SwiftRohan
 
 struct MathNodeLayoutTests {
-
+  @Test
+  func mathNodes_fromScratch() {
+    let mathNodes: [MathNode] = MathNodeTests.allSamples()
+    let styleSheet = StyleSheetTests.sampleStyleSheet()
+    let contentNode = ContentNode(mathNodes)
+    let mathContext = MathUtils.resolveMathContext(for: contentNode, styleSheet)
+    let fragment = MathListLayoutFragment(mathContext)
+    let context = MathListLayoutContext(styleSheet, mathContext, fragment)
+    context.beginEditing()
+    contentNode.performLayout(context, fromScratch: true)
+    context.endEditing()
+  }
 }
