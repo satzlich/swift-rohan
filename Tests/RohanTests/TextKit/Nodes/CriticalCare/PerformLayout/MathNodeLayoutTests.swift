d51d600023e9777397e7afcf6d1a7db0d749cda5
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-03 18:29:24 +0800

    fix

--- Tests/RohanTests/TextKit/Nodes/CriticalCare/PerformLayout/MathNodeLayoutTests.swift
+++ Tests/RohanTests/TextKit/Nodes/CriticalCare/PerformLayout/MathNodeLayoutTests.swift
@@ -19,4 +19,41 @@ struct MathNodeLayoutTests {
     contentNode.performLayout(context, fromScratch: true)
     context.endEditing()
   }
+
+  private func createTestScene<T: MathNode>(
+    _ node: T
+  ) -> (ContentNode, MathListLayoutContext) {
+    let styleSheet = StyleSheetTests.sampleStyleSheet()
+    let contentNode = ContentNode([node])
+    let mathContext = MathUtils.resolveMathContext(for: contentNode, styleSheet)
+    let fragment = MathListLayoutFragment(mathContext)
+    let context = MathListLayoutContext(styleSheet, mathContext, fragment)
+    context.beginEditing()
+    contentNode.performLayout(context, fromScratch: true)
+    context.endEditing()
+    return (contentNode, context)
+  }
+
+  private func performLayout(
+    _ context: MathListLayoutContext, _ contentNode: ContentNode
+  ) {
+    context.resetCursor()
+    context.beginEditing()
+    contentNode.performLayout(context, fromScratch: false)
+    context.endEditing()
+  }
+
+  @Test
+  func accent() {
+    let accentNode = AccentNode(.overleftarrow, nucleus: [TextNode("x")])
+    let (contentNode, context) = createTestScene(accentNode)
+    do {
+      accentNode.nucleus.replaceChild(TextNode("x"), at: 0, inStorage: true)
+      performLayout(context, contentNode)
+    }
+    do {
+      accentNode.nucleus.replaceChild(TextNode("xz"), at: 0, inStorage: true)
+      performLayout(context, contentNode)
+    }
+  }
 }
