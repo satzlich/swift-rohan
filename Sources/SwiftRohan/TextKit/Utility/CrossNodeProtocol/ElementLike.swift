71d6e4b2dd1c1886086a8bae301163c4301e37e5
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-06 10:43:19 +0800

    doc

--- Sources/SwiftRohan/TextKit/Utility/CrossNodeProtocol/ElementLike.swift
+++ Sources/SwiftRohan/TextKit/Utility/CrossNodeProtocol/ElementLike.swift
@@ -2,9 +2,14 @@
 
 /// Common interface for nodes that can have linear children.
 protocol ElementLike: Node {
+  /// The number of child nodes.
   var childCount: Int { get }
+
+  /// Returns the child node at the specified index.
+  /// - Precondition: `index` must be within the bounds of the children.
   func getChild(_ index: Int) -> Node
 
+  /// Accepts a visitor to visit the given children in the manner of this node.
   func accept<R, C, V: NodeVisitor<R, C>, T: NodeLike, S: Collection<T>>(
     _ visitor: V, _ context: C, withChildren children: S
   ) -> R
