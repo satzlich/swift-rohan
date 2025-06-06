cdb280f8098e90413b044bc1556c07a8926d2f93
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-06 12:43:05 +0800

    pol

--- Sources/SwiftRohan/TextKit/Layout/LayoutContext/MathReflowLayoutContext.swift
+++ Sources/SwiftRohan/TextKit/Layout/LayoutContext/MathReflowLayoutContext.swift
@@ -26,44 +26,105 @@ final class MathReflowLayoutContext: LayoutContext {
   var isEditing: Bool { mathListLayoutContext.isEditing }
 
   func beginEditing() {
+    beginReflow()
     mathListLayoutContext.beginEditing()
   }
 
   func endEditing() {
     mathListLayoutContext.endEditing()
+    commitReflow()
   }
 
   // MARK: - Layout
 
   func addParagraphStyle(_ source: Node, _ range: Range<Int>) {
+    precondition(isEditing)
     mathListLayoutContext.addParagraphStyle(source, range)
   }
 
   func skipBackwards(_ n: Int) {
+    precondition(isEditing)
     mathListLayoutContext.skipBackwards(n)
   }
 
   func deleteBackwards(_ n: Int) {
+    precondition(isEditing)
     mathListLayoutContext.deleteBackwards(n)
   }
 
   func invalidateBackwards(_ n: Int) {
+    precondition(isEditing)
     mathListLayoutContext.invalidateBackwards(n)
   }
 
   func insertText<S: Collection<Character>>(_ text: S, _ source: Node) {
+    precondition(isEditing)
     mathListLayoutContext.insertText(text, source)
   }
 
   func insertNewline(_ context: Node) {
+    precondition(isEditing)
     mathListLayoutContext.insertNewline(context)
   }
 
   func insertFragment(_ fragment: any LayoutFragment, _ source: Node) {
+    precondition(isEditing)
     mathListLayoutContext.insertFragment(fragment, source)
   }
 
   // MARK: - Query
+
+  func getSegmentFrame(
+    for layoutOffset: Int, _ affinity: RhTextSelection.Affinity, _ node: Node
+  ) -> SegmentFrame? {
+    precondition(!isEditing)
+    let convertedOffset = reflowedOffset(for: layoutOffset)
+    return textLayoutContext.getSegmentFrame(for: convertedOffset, affinity, node)
+  }
+
+  func enumerateTextSegments(
+    _ layoutRange: Range<Int>, type: DocumentManager.SegmentType,
+    options: DocumentManager.SegmentOptions,
+    using block: (Range<Int>?, CGRect, CGFloat) -> Bool
+  ) -> Bool {
+    precondition(!isEditing)
+    let convertedRange = reflowedRange(for: layoutRange)
+    return textLayoutContext.enumerateTextSegments(
+      convertedRange, type: type, options: options, using: block)
+  }
+
+  func getLayoutRange(interactingAt point: CGPoint) -> PickingResult? {
+    precondition(!isEditing)
+    if let result = textLayoutContext.getLayoutRange(interactingAt: point) {
+      let restoredRange = originalRange(for: result.layoutRange)
+      return result.with(layoutRange: restoredRange)
+    }
+    else {
+      return nil
+    }
+  }
+
+  func rayshoot(
+    from layoutOffset: Int, affinity: RhTextSelection.Affinity,
+    direction: TextSelectionNavigation.Direction
+  ) -> RayshootResult? {
+    precondition(!isEditing)
+    let convertedOffset = reflowedOffset(for: layoutOffset)
+    return textLayoutContext.rayshoot(
+      from: convertedOffset, affinity: affinity, direction: direction)
+  }
+
+  func lineFrame(
+    from layoutOffset: Int, affinity: RhTextSelection.Affinity,
+    direction: TextSelectionNavigation.Direction
+  ) -> SegmentFrame? {
+    precondition(!isEditing)
+    let convertedOffset = reflowedOffset(for: layoutOffset)
+    return textLayoutContext.lineFrame(
+      from: convertedOffset, affinity: affinity, direction: direction)
+  }
+
+  // MARK: - Reflow
 
   private func reflowedOffset(for layoutOffset: Int) -> Int {
     preconditionFailure()
@@ -81,48 +142,15 @@ final class MathReflowLayoutContext: LayoutContext {
     preconditionFailure()
   }
 
-  func getSegmentFrame(
-    for layoutOffset: Int, _ affinity: RhTextSelection.Affinity, _ node: Node
-  ) -> SegmentFrame? {
-    let convertedOffset = reflowedOffset(for: layoutOffset)
-    return textLayoutContext.getSegmentFrame(for: convertedOffset, affinity, node)
+  /// Begin a reflow operation.
+  private func beginReflow() {
+    // Implementation: remove previous reflowed segments
+    preconditionFailure()
   }
 
-  func enumerateTextSegments(
-    _ layoutRange: Range<Int>, type: DocumentManager.SegmentType,
-    options: DocumentManager.SegmentOptions,
-    using block: (Range<Int>?, CGRect, CGFloat) -> Bool
-  ) -> Bool {
-    let convertedRange = reflowedRange(for: layoutRange)
-    return textLayoutContext.enumerateTextSegments(
-      convertedRange, type: type, options: options, using: block)
-  }
-
-  func getLayoutRange(interactingAt point: CGPoint) -> PickingResult? {
-    if let result = textLayoutContext.getLayoutRange(interactingAt: point) {
-      let restoredRange = originalRange(for: result.layoutRange)
-      return result.with(layoutRange: restoredRange)
-    }
-    else {
-      return nil
-    }
-  }
-
-  func rayshoot(
-    from layoutOffset: Int, affinity: RhTextSelection.Affinity,
-    direction: TextSelectionNavigation.Direction
-  ) -> RayshootResult? {
-    let convertedOffset = reflowedOffset(for: layoutOffset)
-    return textLayoutContext.rayshoot(
-      from: convertedOffset, affinity: affinity, direction: direction)
-  }
-
-  func lineFrame(
-    from layoutOffset: Int, affinity: RhTextSelection.Affinity,
-    direction: TextSelectionNavigation.Direction
-  ) -> SegmentFrame? {
-    let convertedOffset = reflowedOffset(for: layoutOffset)
-    return textLayoutContext.lineFrame(
-      from: convertedOffset, affinity: affinity, direction: direction)
+  /// Commit the reflow operation.
+  private func commitReflow() {
+    // Implementation: insert reflowed segments into the layout context
+    preconditionFailure()
   }
 }
