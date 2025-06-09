669094369f0e5dfc78430a3c1cc0084d3b97c991
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-09 17:18:00 +0800

    (WIP) refactor

---
+++ Sources/SwiftRohan/TextKit/Utility/Operation/InsertionResult.swift
@@ -0,0 +1,21 @@
+// Copyright 2024-2025 Lie Yan
+
+enum InsertionResult<T> {
+  /// insertion is successful.
+  case inserted(T)
+  /// insertion is successful with a new paragraph holding the inserted content.
+  case paragraphInserted(T)
+  
+  case failure(SatzError)
+
+  func map<U>(_ transform: (T) throws -> U) rethrows -> InsertionResult<U> {
+    switch self {
+    case .inserted(let value):
+      return .inserted(try transform(value))
+    case .paragraphInserted(let value):
+      return .paragraphInserted(try transform(value))
+    case .failure(let error):
+      return .failure(error)
+    }
+  }
+}
