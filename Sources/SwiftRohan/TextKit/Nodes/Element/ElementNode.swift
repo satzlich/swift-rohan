b2bee5f59650e3f4316f69fac32994a361c7fe81
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-15 19:52:19 +0800

    pol

--- Sources/SwiftRohan/TextKit/Nodes/Element/ElementNode.swift
+++ Sources/SwiftRohan/TextKit/Nodes/Element/ElementNode.swift
@@ -317,8 +317,14 @@ internal class ElementNode: Node {
 
         func fallbackLastIndex() {
           let fraction = computeFraction(consumed, child)
-          let resolved = index + (fraction > 0.5 ? 1 : 0)
-          trace.moveTo(.index(resolved))
+          if fraction > 0.5 {
+            trace.moveTo(.index(index + 1))
+            affinity = .upstream
+          }
+          else {
+            trace.moveTo(.index(index))
+            affinity = .downstream
+          }
         }
 
         switch child {
