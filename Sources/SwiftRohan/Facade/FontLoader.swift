f656fa6091d637761b27a056a9653843d6a502d6
Lie Yan <robyn.lie.yan@outlook.com>
2025-06-16 11:49:35 +0800

    polish FontLoader

--- Sources/SwiftRohan/Facade/FontLoader.swift
+++ Sources/SwiftRohan/Facade/FontLoader.swift
@@ -11,91 +11,86 @@ public enum FontLoader {
         assertionFailure("Invalid font file name: \(font)")
         continue
       }
-      let name = parts[0]
-      let ext = parts[1]
-      registerFont(named: name, extension: ext)
+      _registerFont(named: parts[0], extension: parts[1])
     }
   }
 
-  /// Registers a single font file
-  private static func registerFont(
+  private static func _registerFont(
     named name: String, extension extensionName: String
   ) {
-    guard
-      let fontURL = Bundle.module.url(forResource: name, withExtension: extensionName),
-      let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-      let font = CGFont(fontDataProvider)
+    guard let fontURL = Bundle.module.url(forResource: name, withExtension: extensionName)
     else {
       assertionFailure("Failed to load font: \(name)")
       return
     }
-
     var error: Unmanaged<CFError>?
-    if !CTFontManagerRegisterGraphicsFont(font, &error) {
+    if !CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, &error) {
       let error = error?.takeUnretainedValue().localizedDescription ?? "Unknown error"
       assertionFailure("Failed to register font: \(name), error: \(error)")
     }
   }
 
-  static let allFonts: Array<String> =
-    ConcreteMath + CMUConcrete + Libertinus + Noto + newComputerModern + STIX
+  static let allFonts: Array<String> = _allFonts()
 
-  private static let ConcreteMath: Array<String> = [
-    "Concrete-Math.otf",
-    "Concrete-Math-Bold.otf",
-  ]
+  private static func _allFonts() -> Array<String> {
+    let ConcreteMath: Array<String> = [
+      "Concrete-Math.otf",
+      "Concrete-Math-Bold.otf",
+    ]
+    let CMUConcrete: Array<String> = [
+      "cmunobi.ttf", "cmunobx.ttf", "cmunorm.ttf", "cmunoti.ttf",
+    ]
 
-  private static let CMUConcrete: Array<String> = [
-    "cmunobi.ttf", "cmunobx.ttf", "cmunorm.ttf", "cmunoti.ttf",
-  ]
+    let Libertinus: Array<String> = [
+      "LibertinusMath-Regular.otf",
+      "LibertinusMono-Regular.otf",
+      "LibertinusSans-Bold.otf",
+      "LibertinusSans-Italic.otf",
+      "LibertinusSans-Regular.otf",
+      "LibertinusSerif-Bold.otf",
+      "LibertinusSerif-BoldItalic.otf",
+      "LibertinusSerif-Italic.otf",
+      "LibertinusSerif-Regular.otf",
+    ]
 
-  private static let Libertinus: Array<String> = [
-    "LibertinusMath-Regular.otf",
-    "LibertinusMono-Regular.otf",
-    "LibertinusSans-Bold.otf",
-    "LibertinusSans-Italic.otf",
-    "LibertinusSans-Regular.otf",
-    "LibertinusSerif-Bold.otf",
-    "LibertinusSerif-BoldItalic.otf",
-    "LibertinusSerif-Italic.otf",
-    "LibertinusSerif-Regular.otf",
-  ]
+    let Noto: Array<String> = [
+      "NotoSans-Bold.ttf",
+      "NotoSans-BoldItalic.ttf",
+      "NotoSans-Italic.ttf",
+      "NotoSans-Regular.ttf",
+      "NotoSerif-Bold.ttf",
+      "NotoSerif-BoldItalic.ttf",
+      "NotoSerif-Italic.ttf",
+      "NotoSerif-Regular.ttf",
+      "NotoSansMath-Regular.ttf",
+    ]
 
-  private static let Noto: Array<String> = [
-    "NotoSans-Bold.ttf",
-    "NotoSans-BoldItalic.ttf",
-    "NotoSans-Italic.ttf",
-    "NotoSans-Regular.ttf",
-    "NotoSerif-Bold.ttf",
-    "NotoSerif-BoldItalic.ttf",
-    "NotoSerif-Italic.ttf",
-    "NotoSerif-Regular.ttf",
-    "NotoSansMath-Regular.ttf",
-  ]
+    let NewComputerModern: Array<String> = [
+      "NewCM10-Bold.otf",
+      "NewCM10-BoldItalic.otf",
+      "NewCM10-Italic.otf",
+      "NewCM10-Regular.otf",
+      "NewCMMath-Bold.otf",
+      "NewCMMath-Regular.otf",
+      "NewCMMono10-Bold.otf",
+      "NewCMMono10-BoldOblique.otf",
+      "NewCMMono10-Italic.otf",
+      "NewCMMono10-Regular.otf",
+      "NewCMSans10-Bold.otf",
+      "NewCMSans10-BoldOblique.otf",
+      "NewCMSans10-Oblique.otf",
+      "NewCMSans10-Regular.otf",
+      "NewCMSansMath-Regular.otf",
+    ]
 
-  private static let newComputerModern: Array<String> = [
-    "NewCM10-Bold.otf",
-    "NewCM10-BoldItalic.otf",
-    "NewCM10-Italic.otf",
-    "NewCM10-Regular.otf",
-    "NewCMMath-Bold.otf",
-    "NewCMMath-Regular.otf",
-    "NewCMMono10-Bold.otf",
-    "NewCMMono10-BoldOblique.otf",
-    "NewCMMono10-Italic.otf",
-    "NewCMMono10-Regular.otf",
-    "NewCMSans10-Bold.otf",
-    "NewCMSans10-BoldOblique.otf",
-    "NewCMSans10-Oblique.otf",
-    "NewCMSans10-Regular.otf",
-    "NewCMSansMath-Regular.otf",
-  ]
+    let STIX: Array<String> = [
+      "STIXTwoMath-Regular.otf",
+      "STIXTwoText-Bold.otf",
+      "STIXTwoText-BoldItalic.otf",
+      "STIXTwoText-Italic.otf",
+      "STIXTwoText-Regular.otf",
+    ]
 
-  private static let STIX: Array<String> = [
-    "STIXTwoMath-Regular.otf",
-    "STIXTwoText-Bold.otf",
-    "STIXTwoText-BoldItalic.otf",
-    "STIXTwoText-Italic.otf",
-    "STIXTwoText-Regular.otf",
-  ]
+    return ConcreteMath + CMUConcrete + Libertinus + Noto + NewComputerModern + STIX
+  }
 }
