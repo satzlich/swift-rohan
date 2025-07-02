39d55aaa3276bce9da9471a520bd4cf6755a3d39
Lie Yan <robyn.lie.yan@outlook.com>
2025-07-02 12:16:47 +0800

    extend counter segment

--- Sources/SwiftRohan/Foundation/Counter/CounterSegment.swift
+++ Sources/SwiftRohan/Foundation/Counter/CounterSegment.swift
@@ -1,7 +1,7 @@
 // Copyright 2024-2025 Lie Yan
 
 /// A segment of `CountHolder`s that is delimited by `[begin, end]` (inclusive).
-struct CounterSegment {
+struct CounterSegment: Equatable, Hashable {
   let begin: CountHolder
   let end: CountHolder
 
@@ -15,5 +15,14 @@ struct CounterSegment {
   init(_ holder: CountHolder) {
     self.begin = holder
     self.end = holder
+  }
+
+  static func == (lhs: CounterSegment, rhs: CounterSegment) -> Bool {
+    lhs.begin === rhs.begin && lhs.end === rhs.end
+  }
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(ObjectIdentifier(begin))
+    hasher.combine(ObjectIdentifier(end))
   }
 }
