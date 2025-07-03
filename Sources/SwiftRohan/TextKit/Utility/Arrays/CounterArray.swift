8e8ac3f9b4e5f0af96f49ebe82bd33b43805b899
Lie Yan <robyn.lie.yan@outlook.com>
2025-07-03 20:23:34 +0800

    poli

--- Sources/SwiftRohan/TextKit/Utility/Arrays/CounterArray.swift
+++ Sources/SwiftRohan/TextKit/Utility/Arrays/CounterArray.swift
@@ -1,60 +1,65 @@
 // Copyright 2024-2025 Lie Yan
 
+/// Simple wrapper over BoolArray for managing counter segments.
 internal struct CounterArray {
-  private var _boolArray: BoolArray
+  @usableFromInline
+  internal var _boolArray: BoolArray
 
+  @inlinable @inline(__always)
   init(_ counterSegments: some Collection<CounterSegment?>) {
     self._boolArray = BoolArray(counterSegments.lazy.map { $0 != nil })
   }
 
+  @inlinable @inline(__always)
   init() {
     self._boolArray = BoolArray()
   }
 
-  @inline(__always) var isEmpty: Bool { _boolArray.isEmpty }
-  @inline(__always) var count: Int { _boolArray.count }
-  @inline(__always) var trueCount: Int { _boolArray.trueCount }
+  @inlinable @inline(__always) var isEmpty: Bool { _boolArray.isEmpty }
+  @inlinable @inline(__always) var count: Int { _boolArray.count }
+  @inlinable @inline(__always) var trueCount: Int { _boolArray.trueCount }
 
-  subscript(index: Int) -> Bool { @inline(__always) get { _boolArray[index] } }
+  @inlinable @inline(__always)
+  subscript(index: Int) -> Bool { _boolArray[index] }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   func trueIndex(before position: Int) -> Int? {
     _boolArray.trueIndex(before: position)
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   func trueIndex(after position: Int) -> Int? {
     _boolArray.trueIndex(after: position)
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   mutating func insert(_ segment: CounterSegment?, at index: Int) {
     _boolArray.insert(segment != nil, at: index)
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   mutating func insert(
     contentsOf segments: some Collection<CounterSegment?>, at index: Int
   ) {
     _boolArray.insert(contentsOf: segments.lazy.map { $0 != nil }, at: index)
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   mutating func remove(at index: Int) -> Bool {
     _boolArray.remove(at: index)
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   mutating func removeSubrange(_ range: Range<Int>) {
     _boolArray.removeSubrange(range)
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   mutating func removeAll() {
     _boolArray.removeAll()
   }
 
-  @inline(__always)
+  @inlinable @inline(__always)
   mutating func replaceSubrange(
     _ subrange: Range<Int>, with newElements: some Collection<CounterSegment?>
   ) {
