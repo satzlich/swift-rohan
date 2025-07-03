8eb0e80259cac5cb4098991399fb61fe7538b7ae
Lie Yan <robyn.lie.yan@outlook.com>
2025-07-03 09:19:55 +0800

    refactor: add counter segment

--- Sources/SwiftRohan/Foundation/Counter/CountHolder/CountHolder.swift
+++ Sources/SwiftRohan/Foundation/Counter/CountHolder/CountHolder.swift
@@ -27,61 +27,83 @@ internal class CountHolder {
   }
 
   static func insert(_ holder: CountHolder, before next: CountHolder) {
-    precondition(next.previous != nil)
-    let p = next.previous!
-
-    holder.previous = p
-    p.next = holder
-    next.previous = holder
-    holder.next = next
+    if let p = next.previous {
+      holder.previous = p
+      p.next = holder
+      next.previous = holder
+      holder.next = next
+    }
+    else {
+      // `next` is the first holder in the linked list.
+      holder.next = next
+      next.previous = holder
+    }
   }
 
   static func insert(
-    contentsOf holders: some Collection<CountHolder>, before next: CountHolder
+    contentsOf holders: some BidirectionalCollection<CountHolder>,
+    before next: CountHolder
   ) {
-    precondition(next.previous != nil)
     guard holders.isEmpty == false else { return }
 
-    var p = next.previous!
+    if var p = next.previous {
 
-    for holder in holders {
-      holder.previous = p
-      p.next = holder
-      p = holder
+      for holder in holders {
+        holder.previous = p
+        p.next = holder
+        p = holder
+      }
+
+      next.previous = p
+      p.next = next
     }
+    else {
+      // `next` is the first holder in the linked list.
 
-    next.previous = p
-    p.next = next
+      for (p, q) in holders.adjacentPairs() {
+        p.next = q
+        q.previous = p
+      }
+      let last = holders.last!
+      last.next = next
+      next.previous = last
+    }
   }
 
   static func remove(_ holder: CountHolder) {
-    precondition(holder.previous != nil)
-
-    let p = holder.previous!
-
-    p.next = holder.next
-    holder.next?.previous = p
-    holder.previous = nil
-    holder.next = nil
+    if let p = holder.previous {
+      p.next = holder.next
+      holder.next?.previous = p
+      holder.previous = nil
+      holder.next = nil
+    }
+    else {
+      // `holder` is the first holder in the linked list.
+      guard let next = holder.next else { return }
+      next.previous = nil
+      holder.next = nil
+    }
   }
 
   /// Remove the count holders in the half-open range `[begin, end)`.
   static func removeSubrange(_ begin: CountHolder, _ end: CountHolder) {
-    precondition(begin.previous != nil)
-
     guard begin !== end else { return }
 
-    begin.previous?.next = end
-    end.previous?.next = nil
-    end.previous = begin.previous
-    begin.previous = nil
+    if begin.previous != nil {
+      begin.previous?.next = end
+      end.previous?.next = nil
+      end.previous = begin.previous
+      begin.previous = nil
+    }
+    else {
+      // `[begin,end)` is the initial segment in the linked list.
+      end.previous?.next = nil
+      end.previous = nil
+    }
   }
 
   /// Remove the count holders in the closed range `[begin, end]`.
   static func removeSubrange(_ begin: CountHolder, inclusive end: CountHolder) {
-    precondition(begin.previous != nil)
-    precondition(end.next != nil)
-
     guard let end = end.next else { return }
     removeSubrange(begin, end)
   }
