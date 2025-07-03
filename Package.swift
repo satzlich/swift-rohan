30ab4d93a8587b5a9c8702e2c6b66cb3f339a115
Lie Yan <robyn.lie.yan@outlook.com>
2025-07-03 22:34:13 +0800

    upgrade dependency

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
-    .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.2"),
+    .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.1"),
+    .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-syntax", from: "510.0.0"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", from: "510.0.0"),
     .package(url: "https://github.com/satzlich/satz-algorithms", branch: "main"),
     .package(url: "https://github.com/satzlich/swift-ttf-parser", branch: "main"),
     .package(url: "https://github.com/satzlich/swift-unicode-math", from: "1.0.0"),
@@ -55,7 +55,7 @@ let package = Package(
         // .define("DECORATE_LAYOUT_FRAGMENT"),
         // .define("COLLECT_STATS_FRAGMENT_VIEW_CACHE"),
         // .define("LOG_MARKED_TEXT"),
-        .define("LOG_TEXT_SELECTION"),
+        .define("LOG_TEXT_SELECTION")
         // .define("LOG_PICKING_POINT"),
         // .define("SIMULATE_COMPLETION_DELAY"),
       ],
